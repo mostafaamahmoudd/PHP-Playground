e4639cc3462527d9edcf4644abbb99a5a228c671
Mostafa <mostafamahmoud805554@std.mans.edu.eg>
2024-08-04 15:16:00 +0300

    partials

---
+++ partials/views/about.view.php
@@ -0,0 +1,11 @@
+<?php require "partials/header.php" ?>
+<?php require "partials/nav.php" ?>
+<?php require "partials/banner.php" ?>
+
+    <main>
+        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+            <p>Now you're in the About page!</p>
+        </div>
+    </main>
+
+<?php require "partials/footer.php" ?>
