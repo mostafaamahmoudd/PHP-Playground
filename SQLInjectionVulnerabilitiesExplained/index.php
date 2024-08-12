e4715ae0199e38cdbbd5e0f957d175e140acd4dc
Mostafa <mostafamahmoud805554@std.mans.edu.eg>
2024-08-12 18:40:39 +0300

    SQL Injection Vulnerabilities Explained

---
+++ SQLInjectionVulnerabilitiesExplained/index.php
@@ -0,0 +1,15 @@
+<?php
+
+require 'functions.php';
+require 'Database.php';
+//require 'router.php';
+
+$config = require 'config.php';
+$db = new Database($config['database']);
+
+$id = $_GET['id'];
+$query = "select * from posts where id = :id";
+
+$posts = $db->query($query, [':id' => $id])->fetch();
+
+dd($posts['title']);
