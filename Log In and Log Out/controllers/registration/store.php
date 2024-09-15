b7990ddcefab74bbd43308f105676c7cc9b33099
Mostafa <mostafamahmoud805554@std.mans.edu.eg>
2024-09-15 12:59:02 +0300

    Log In and Log Out

---
+++ Log In and Log Out/controllers/registration/store.php
@@ -0,0 +1,45 @@
+<?php
+
+use Core\App;
+use Core\Database;
+use Core\Validator;
+
+$db = App::resolve(Database::class);
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$errors = [];
+if (!Validator::email($email)) {
+    $errors['email'] = 'Please provide a valid email address.';
+}
+
+if (!Validator::string($password, 7, 255)) {
+    $errors['password'] = 'Please provide a valid password of at least seven characters.';
+}
+
+if (!empty($errors)) {
+    return view('registration/create.view.php', [
+        'errors' => $errors
+    ]);
+}
+
+$user = $db->query('select * from users where email = :email', [
+    'email' => $email
+])->find();
+
+if ($user) {
+    header('locationL: /');
+    exit();
+} else {
+    $db->query('INSERT INTO users(id, email, password) VALUES(:id, :email, :password)', [
+        'id' => 2,
+        'email' => $email,
+        'password' => password_hash($password, PASSWORD_BCRYPT)
+    ]);
+
+    login($user);
+
+    header('location: /');
+    exit();
+}
