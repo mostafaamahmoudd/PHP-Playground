54a3e6e4640bbc17c18cbd62b7342fc211be89bf
mostafaamahmoudd <mostafaa.mahmoudd550@gmail.com>
2025-08-11 13:35:15 +0300

    singleton design pattern

---
+++ DesignPatterns/Creational/Singleton/AppSettings.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Mustafa\Temp;
+
+class AppSettings
+{
+    private static array $config;
+    private static ?self $instance = null;
+
+    private function __construct()
+    {
+        self::$config = parse_ini_file(__DIR__.'/config.ini', true);
+    }
+
+    private function __clone() {}
+
+    public static function getInstance(): self
+    {
+        if (is_null(self::$instance)) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+
+    public static function getConfig(string $key)
+    {
+        return self::$config[$key] ?? null;
+    }
+}
